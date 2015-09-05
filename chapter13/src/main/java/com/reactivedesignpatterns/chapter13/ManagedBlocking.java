0009e32327cba73f0ede51a81c892e2ec1849aee
Roland Kuhn <rk@rkuhn.info>
2015-09-05 11:20:24 +0200

    finish managed blocking

--- chapter13/src/main/java/com/reactivedesignpatterns/chapter13/ManagedBlocking.java
+++ chapter13/src/main/java/com/reactivedesignpatterns/chapter13/ManagedBlocking.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -45,15 +45,15 @@ public interface ManagedBlocking {
 		public final String credentials;
 		public final AccessRights[] rights;
 
-		public CheckAccessResult(String username, String credentials, AccessRights[] rights) {
-			this.username = username;
-			this.credentials = credentials;
+		public CheckAccessResult(CheckAccess ca, AccessRights[] rights) {
+			this.username = ca.username;
+			this.credentials = ca.credentials;
 			this.rights = rights;
 		}
 	}
 	
 	public class AccessService extends AbstractActor {
-		private Executor pool;
+		private ExecutorService pool;
 		
 		public AccessService(DataSource db, int poolSize, int queueSize) {
 			pool = new ThreadPoolExecutor(0, poolSize, 60, SECONDS, new LinkedBlockingDeque<>(queueSize));
@@ -64,22 +64,26 @@ public interface ManagedBlocking {
 					try {
 						pool.execute(() -> checkAccess(db, ca, self));
 					} catch (RejectedExecutionException e) {
-						ca.replyTo.tell(new CheckAccessResult(ca.username, ca.credentials, AccessRights.EMPTY), self);
+						ca.replyTo.tell(new CheckAccessResult(ca, AccessRights.EMPTY), self);
 					}})
 				.build());
 		}
 		
+		@Override
+		public void postStop() {
+			pool.shutdownNow();
+		}
+		
 		private static void checkAccess(DataSource db, CheckAccess ca, ActorRef self) {
-			try {
-				final Connection conn = db.getConnection();
+			try (Connection conn = db.getConnection()) {
 				final ResultSet result = conn.createStatement().executeQuery("<figure out access rights>");
 				final List<AccessRights> rights = new LinkedList<>();
 				while (result.next()) {
 					rights.add(AccessRights.valueOf(result.getString(0)));
 				}
-				ca.replyTo.tell(new CheckAccessResult(ca.username, ca.credentials, rights.toArray(AccessRights.EMPTY)), self);
+				ca.replyTo.tell(new CheckAccessResult(ca, rights.toArray(AccessRights.EMPTY)), self);
 			} catch (Exception e) {
-				ca.replyTo.tell(new CheckAccessResult(ca.username, ca.credentials, AccessRights.EMPTY), self);
+				ca.replyTo.tell(new CheckAccessResult(ca, AccessRights.EMPTY), self);
 			}
 		}
 	}
