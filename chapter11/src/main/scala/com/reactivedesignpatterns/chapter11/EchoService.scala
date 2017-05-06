cd22f7d18772bf7db1094a3a026ccd52cde58bf9
phderome <pderome@postmedia.com>
2017-05-06 08:03:34 -0400

    rename chapter04 to chapter11 as per book structure (#10)
    * Moved RateLimiter.scala (without any CircuitBreaker mind you) back to Chapter12 as per book
    Fixed sbt.IO dependency in build.scala and chapter13 (must have been in .ivy cache for RKuhn), removed some unused imports
    directory change for MultiMasterCRDTSpec.scala (now chapter13)
    
    * moved contents of chapter04 to chapter11 to reflect published physical book contents
    
    * moved contents of chapter04 to chapter11 to reflect published physical book contents
    
    * undid the previous commits to ensure we rebase properly
    
    * moving chapter04 to chapter11 as per book

--- chapter11/src/main/scala/com/reactivedesignpatterns/chapter11/EchoService.scala
+++ chapter11/src/main/scala/com/reactivedesignpatterns/chapter11/EchoService.scala
@@ -1,7 +1,6 @@
-package com.reactivedesignpatterns.chapter4
+package com.reactivedesignpatterns.chapter11
 
-import akka.actor.Actor
-import akka.actor.ActorRef
+import akka.actor.{Actor, ActorRef}
 
 object EchoService {
   case class Request(tag: String, client: ActorRef)
