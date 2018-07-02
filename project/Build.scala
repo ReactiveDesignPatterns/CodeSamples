ebfaf90cddd8d351236a24f22d5acf019aec6be4
虎鸣 <hepin.p@alibabapictures.com>
2018-07-02 14:10:54 +0800

    update dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -32,14 +32,14 @@ object Build {
 
   val levelDb = "org.iq80.leveldb" % "leveldb" % "0.10"
 
-  val amazonAWS = "com.amazonaws" % "aws-java-sdk" % "1.11.350"
+  val amazonAWS = "com.amazonaws" % "aws-java-sdk" % "1.11.358"
 
   val sbtIO = "org.scala-sbt" %% "io" % "1.1.10"
 
   val scalaAsync = "org.scala-lang.modules" %% "scala-async" % "0.9.7"
   val scalaJava8 = "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.0"
 
-  val rxJava = "io.reactivex.rxjava2" % "rxjava" % "2.1.15"
+  val rxJava = "io.reactivex.rxjava2" % "rxjava" % "2.1.16"
 
   val playJson = "com.typesafe.play" %% "play-json" % "2.6.9"
 
