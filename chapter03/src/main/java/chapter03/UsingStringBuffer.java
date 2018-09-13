69f77192ae1efbee874c6a06c9dc81a71dfe105f
虎鸣 <hepin.p@alibabapictures.com>
2018-09-14 04:31:08 +0800

    = chapter3 make it left margin 2

--- chapter03/src/main/java/chapter03/UsingStringBuffer.java
+++ chapter03/src/main/java/chapter03/UsingStringBuffer.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018 https://www.reactivedesignpatterns.com/
- * 
+ *
  * Copyright (c) 2018 https://rdp.reactiveplatform.xyz/
  */
 
@@ -13,9 +13,9 @@ public class UsingStringBuffer {
     final StringBuffer original = new StringBuffer("foo");
     final StringBuffer reversed = original.reverse();
     System.out.println(String.format(
-        "original '%s', new value '%s'",
-        original,
-        reversed));
+      "original '%s', new value '%s'",
+      original,
+      reversed));
     // #snip
   }
 }
