8ead70b495c9df2327e7d45eb83cfe71f93b3a03
hepin1989 <hepin1989@gmail.com>
2018-12-29 04:30:41 +0800

    Cross java support and update rules.

--- project/Build.scala
+++ project/Build.scala
@@ -31,6 +31,9 @@ object Build {
     )
   )
 
+  final val DefaultScalacOptions = Seq("-encoding", "UTF-8", "-feature", "-unchecked", "-Xlog-reflective-calls", "-Xlint", "-Ywarn-unused")
+  final val DefaultJavacOptions = Seq("-encoding", "UTF-8", "-Xlint:unchecked", "-XDignore.symbol.file")
+
   val sharedSettings: Seq[Def.Setting[_]] = formats ++ fixes ++ Seq(
     headerLicense := Some(HeaderLicense.Custom(
       s"""|Copyright (c) 2018 https://www.reactivedesignpatterns.com/ ${"\n"}
@@ -39,7 +42,31 @@ object Build {
           |""".stripMargin
     )),
     scalaVersion := "2.12.8",
-    conflictWarning := conflictWarning.value.copy(failOnConflict = false)
+    conflictWarning := conflictWarning.value.copy(failOnConflict = false),
+    scalacOptions in Compile ++= (
+      if (System.getProperty("java.version").startsWith("1."))
+        Seq("-target:jvm-1.8")
+      else if (scalaBinaryVersion.value == "2.11")
+        Seq("-target:jvm-1.8", "-javabootclasspath", CrossJava.Keys.fullJavaHomes.value("8") + "/jre/lib/rt.jar")
+      else
+      // -release 8 is not enough, for some reason we need the 8 rt.jar explicitly #25330
+        Seq("-release", "8", "-javabootclasspath", CrossJava.Keys.fullJavaHomes.value("8") + "/jre/lib/rt.jar")),
+    scalacOptions in Compile ++= Seq("-deprecation", "-Ywarn-unused"),
+    scalacOptions in Test := (scalacOptions in Test).value.filterNot(opt ⇒
+      opt == "-Xlog-reflective-calls" || opt.contains("genjavadoc")) ++ Seq(
+      "-Ywarn-unused"),
+    javacOptions in compile ++= DefaultJavacOptions ++ (
+      if (System.getProperty("java.version").startsWith("1."))
+        Seq()
+      else
+        Seq("-source", "8", "-target", "8", "-bootclasspath", CrossJava.Keys.fullJavaHomes.value("8") + "/jre/lib/rt.jar")
+      ),
+    javacOptions in test ++= DefaultJavacOptions ++ (
+      if (System.getProperty("java.version").startsWith("1."))
+        Seq()
+      else
+        Seq("-source", "8", "-target", "8", "-bootclasspath", CrossJava.Keys.fullJavaHomes.value("8") + "/jre/lib/rt.jar")
+      ),
 
     //    ,
     //    wartremoverErrors ++= Warts.allBut(
